51e300c699fd8b241ff099a64afba8c5234b81a0
noier-carter <104841813+noier-carter@users.noreply.github.com>
2022-05-03 09:49:13 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+$(".start").click(function() {
+    $(".open").show();
+    $(".splitroad").show();
+    $(".choose").show();
+});
+$(".L").click(function() {
+    $(".L1").show();
+    $(".startL").show();
+    $(".start").hide();
+    $(".open").hide();
+    $(".splitroad").hide();
+    $(".choose").hide();
+    $(".next").show();
+});
+$(".R").click(function() {
+    $(".R1").show();
+    $(".startR").show();
+    $(".start").hide();
+    $(".open").hide();
+    $(".splitroad").hide();
+    $(".choose").hide();
+    $(".next").show();
+});
+$(".next").click(function() {
+    $(".").show();
+});
